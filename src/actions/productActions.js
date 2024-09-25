e1ff7e428597bc63ff0d8723601472d74bca890f
mubin <mubin.ice.ru@gmail.com>
2024-09-26 00:43:39 +0600

    udpate product details query

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -79,7 +79,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
-        const { data } = await axios.get(`/shopbackend/api/products/${id}`)
+        const { data } = await axios.get(`/shopbackend/api/products/${id}/`)
 
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
